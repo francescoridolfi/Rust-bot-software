69511cc8e46b1c6a05488d4be3e747ed24ab54e4
Francesco <ridolfinotv@gmail.com>
2017-11-21 18:12:18 +0100

    update plm

--- rustbotPlugin.py
+++ rustbotPlugin.py
@@ -1,35 +1,25 @@
-import os, sys
+import os, sys, ast
 
 class Manager():
     def load(self,folder)
         paths = os.listdir(folder)
         plugins = []
         for file in paths:
-            if(file.endswith(".py")):
-                dot_pos = len(file)-3
-                plugins.append(file[:dot_pos])
+            if(file.endswith(".rbp")):
+                plugins.append(file)
 
         sys.path.insert(0, folder)
 
         info = {}
         
         for plugin in plugins:
-            pl = __import__(plugin)
+            f = open(plugin,"r")
+            pl = f.read()
+            f.close()
 
-            main = pl.main
-            events = pl.events
-            author = pl.author
-            name = pl.name
-            version = pl.version
-            short_desc = pl.short_desc
+            pl = ast.literal_eval(pl)
 
-            info[plugin]["main"]=main
-            info[plugin]["events"]=events
-            info[plugin]["author"]=author
-            info[plugin]["name"]=name
-            info[plugin]["version"]=version
-            info[plugin]["short_desc"]=short_desc
-
+            info[plugin] = pl
         return info
             
             
